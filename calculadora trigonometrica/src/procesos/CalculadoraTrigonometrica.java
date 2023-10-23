0ab728fdd1ec6bd3a29175224ca3c387a6b5acc6
JuanFelipeVelasquez <137722339+JuanFelipeVelasquez@users.noreply.github.com>
2023-10-23 18:39:07 -0500

    Add files via upload

---
+++ calculadora trigonometrica/src/procesos/CalculadoraTrigonometrica.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package procesos;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class CalculadoraTrigonometrica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
